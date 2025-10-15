d1298a0190f8d00f2c2935cb58ef706ac65b6540
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-10-14 21:58:54 -0400

    add: borrando funciones que no se estan usando

--- backend/src/main/java/com/tucasa/backend/model/service/interfaces/DepartamentoService.java
+++ backend/src/main/java/com/tucasa/backend/model/service/interfaces/DepartamentoService.java
@@ -1,5 +1,4 @@
 package com.tucasa.backend.model.service.interfaces;
-import com.tucasa.backend.model.dto.DepartamentoFiltroDto;
 import com.tucasa.backend.model.dto.DepartamentoRequestDto;
 
 import org.springframework.http.ResponseEntity;
@@ -7,5 +6,4 @@ import org.springframework.http.ResponseEntity;
 public interface DepartamentoService {
     ResponseEntity<?> findAll();
     ResponseEntity<?> create(DepartamentoRequestDto departamento);
-    ResponseEntity<?> filtrar2(DepartamentoFiltroDto filtro);
 }
