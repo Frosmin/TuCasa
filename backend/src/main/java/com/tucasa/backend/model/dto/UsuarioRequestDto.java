91ce16a44d512714a80aea32b7caf2eeede8ae43
Sega1A <sergio.garciacuchallo@gmail.com>
2025-10-16 17:32:56 -0400

    feat: user's register with role

---
+++ backend/src/main/java/com/tucasa/backend/model/dto/UsuarioRequestDto.java
@@ -0,0 +1,19 @@
+package com.tucasa.backend.model.dto;
+
+import com.tucasa.backend.model.enums.TipoUsuario;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioRequestDto {
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String correo;
+    private String contrasenia;
+    private TipoUsuario rol;
+}
