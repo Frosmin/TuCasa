d953f7fa08f72d7c1845de9fc17c2ae9f5ba3e99
Lotiel-Dev <karlos817gerceis@gmail.com>
2025-10-17 13:55:49 -0400

    Refactor el servicio y los tipos de propiedades para mejorar el manejo de la carga útil y la gestión de errores.

--- tucasafront/src/app/publicar/types/property.types.ts
+++ tucasafront/src/app/publicar/types/property.types.ts
@@ -54,13 +54,15 @@ export interface PropertyFormData {
 
 
 export type PropertyPayload = {
-  inmueble: InmuebleData,
-  descripcion: string;  
-  tipoOperacion: OperationType;   
+  inmueble: InmuebleData;
+  descripcion: string;
+  descripcionOferta?: string; // agregar si necesitas enviarlo
+  tipoOperacion: OperationType;
   precio: number;
   moneda: Currency;
   duracion: number | null;
   tipoPago: PaymentType;
+  images?: string[];
 };
 
 export interface InmuebleData {
