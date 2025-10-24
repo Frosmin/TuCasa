1a7305252d79539c67ed0a827263ff506931c4db
Frosmin <simonabastomart@gmail.com>
2025-10-24 19:13:23 -0400

    Refactor MultimediaRequestDto to ensure validation messages are accurate; update MultimediaResponseDto for future use; modify application.properties to set error stacktrace inclusion to always

--- backend/src/main/java/com/tucasa/backend/model/dto/MultimediaRequestDto.java
+++ backend/src/main/java/com/tucasa/backend/model/dto/MultimediaRequestDto.java
@@ -1,12 +1,22 @@
 package com.tucasa.backend.model.dto;
 import com.tucasa.backend.model.enums.TipoMultimedia;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 @Data
 public class MultimediaRequestDto {
+
+    @NotNull(message = "La url es obligarotia")
     private String url;
+
+    @NotNull(message = "El tipo es obligarotia")
     private TipoMultimedia tipo;
+
     private String descripcion;
+
+    @NotNull(message = "Estado de activo es obligarotia")
     private Boolean activo = true;
+
+    @NotNull(message = "Es portada es obligarotia")
     private Boolean esPortada = false;
 }
--- backend/src/main/java/com/tucasa/backend/model/dto/MultimediaResponseDto.java
+++ backend/src/main/java/com/tucasa/backend/model/dto/MultimediaResponseDto.java
@@ -1,5 +1,5 @@
 package com.tucasa.backend.model.dto;
 
 public class MultimediaResponseDto {
-    
+    // para mas despues
 }
