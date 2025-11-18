8ce0c38d8c5494a82edd422c511f45e3aaae9bd8
mejia-87 <mejiasanchez3010@gmail.com>
2025-11-18 11:19:33 -0400

    refactor admin components for modal details

--- tucasafront/src/app/admin/dashboard/page.tsx
+++ tucasafront/src/app/admin/dashboard/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Users, Newspaper, X } from "lucide-react";
-import Image from "next/image";
+import { Users, Newspaper } from "lucide-react";
 import { obtenerSolicitudesPendientes, aprobarSolicitud, rechazarSolicitud, SolicitudAgente } from "../services/adminService";
 import { SearchBar } from "@/app/admin/components/SearchBar";
 import FilterSelect from "@/app/admin/components/FilterSelect";
 import { useToast } from '@/components/Toast';
 import { useAuth } from "@/context/AuthContext";
 import Loading from "@/components/Loading";
+import { useRouter } from "next/navigation";
+import ModalDetails from "../components/ModalDetails";
 
 export default function DashboardAdmin() {
   const { user } = useAuth();
@@ -20,6 +21,7 @@ export default function DashboardAdmin() {
   const [selected, setSelected] = useState('');
   const [filteredData, setFilteredData] = useState<SolicitudAgente[]>([]);
   const { showSuccess, showError } = useToast();
+  const router = useRouter();
 
 
   const solicitudesPanel = [
@@ -62,12 +64,7 @@ export default function DashboardAdmin() {
   }, [searchTerm, selected, solicitudes]);
 
   if (!user || user.rol !== "ADMIN") {
-    return (
-      <div className="text-center py-20">
-        <h1 className="text-6xl font-bold">404</h1>
-        <p className="text-xl">Página no encontrada</p>
-      </div>
-    );
+    return router.push("/");
   }
 
   const abrirModal = (solicitud: SolicitudAgente) => {
@@ -107,7 +104,7 @@ export default function DashboardAdmin() {
     }
   };
 
-  if (loading) return <Loading message="Cargando las solicitudes..." /> ;
+  if (loading) return <Loading message="Cargando las solicitudes..." />;
 
   return (
     <div className="min-h-screen bg-gray-50 pb-20">
@@ -195,109 +192,14 @@ export default function DashboardAdmin() {
           </div>
         </div>
       )}
-
-      {/* MODAL */}
       {modalOpen && solicitudSeleccionada && (
-        <div className="fixed inset-0 bg-transparent flex items-center justify-center z-50  ">
-          <div className="bg-white w-full max-w-2xl border border-gray-300 rounded-lg shadow-md  ">
-            <div className="bg-gray-200 flex items-center justify-between px-6 py-4 border-b">
-              <h3 className="text-xl text-gray-800 font-bold">Detalles de Solicitud</h3>
-              <button onClick={cerrarModal} className="text-gray-500 hover:text-gray-700">
-                <X />
-              </button>
-            </div>
-            <div className="px-6 py-4 space-y-6">
-              <div className="grid grid-cols-2 gap-6">
-                <div>
-                  <p className="font-semibold text-gray-800">Nombre Completo</p>
-                  <p className="text-gray-700">{solicitudSeleccionada.usuario.nombre} {solicitudSeleccionada.usuario.apellido}</p>
-                </div>
-
-                <div>
-                  <p className="font-semibold text-gray-800">Email</p>
-                  <p className="text-gray-700">{solicitudSeleccionada.usuario.correo}</p>
-                </div>
-              </div>
-              <div className="grid grid-cols-2 gap-6">
-                <div>
-                  <p className="font-semibold text-gray-800">Teléfono</p>
-                  <p className="text-gray-700">{solicitudSeleccionada.usuario.telefono}</p>
-                </div>
-
-                <div>
-                  <p className="font-semibold text-gray-800">Dirección</p>
-                  <p className="text-gray-700">{solicitudSeleccionada.usuario.direccion}</p>
-                </div>
-              </div>
-              <div>
-                <p className="font-semibold text-gray-800">Descripción de solicitud</p>
-                <p className="text-gray-700">{solicitudSeleccionada.descripcion}</p>
-              </div>
-              <div>
-                <p className="font-semibold text-gray-800">Experiencia</p>
-                <p className="text-gray-700">{solicitudSeleccionada.experiencia}</p>
-              </div>
-              <div className="grid grid-cols-2 gap-6">
-                <div>
-                  <p className="font-semibold text-gray-800">Licencia</p>
-                  <p className="text-gray-700">{solicitudSeleccionada.matricula}</p>
-                </div>
-                <div>
-                  <p className="font-semibold text-gray-800">Curriculum Vitae(PDF)</p>
-                  <a
-                    href={solicitudSeleccionada.cvPath}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="flex items-center gap-2 text-red-600 hover:text-red-700 hover:underline"
-
-                  >
-
-                    <Image
-                      src="/pdf.png"
-                      alt="PDF Icon"
-                      width={24}
-                      height={24}
-                      className="object-contain"
-                    />
-
-                    {solicitudSeleccionada.cvPath.split(/[/\\]/).pop()}
-                  </a>
-                </div>
-
-              </div>
-
-              <div className="px-6 py-4 bg-gray-50 flex justify-end gap-4">
-                <button
-                  onClick={cerrarModal}
-                  className="px-4 py-2 bg-gray-300 rounded-md hover:bg-gray-400"
-                >
-                  Cerrar
-                </button>
-
-                <button
-                  onClick={handleRechazar}
-                  className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
-                >
-                  Rechazar
-                </button>
-
-                <button
-                  onClick={handleAprobar}
-                  className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600"
-                >
-                  Aprobar
-                </button>
-              </div>
-
-            </div>
-
-
-
-          </div>
-        </div>
-      )
-      }
-
+        <ModalDetails
+          solicitudSeleccionada={solicitudSeleccionada}
+          handleAprobar={handleAprobar}
+          handleRechazar={handleRechazar}
+          cerrarModal={cerrarModal}
+        />
+      )}
     </div >
   );
 }
